aff2f94b64a9d96d3d1e222f713549705ca4b9a5
iris2108 <iris2107007@gmail.com>
2025-10-10 13:02:43 +0700

    TS00916 - Lê Vân Như - Lab5

--- Lab4/src/main/java/poly/edu/entity/Staff.java
+++ Lab4/src/main/java/poly/edu/entity/Staff.java
@@ -1,5 +1,6 @@
 package poly.edu.entity;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -10,22 +11,29 @@ import java.util.Date;
 @Builder
 @Data
 public class Staff {
+
+    @NotBlank(message = "Chưa nhập email")
+    @Email(message = "Email không đúng định dạng")
     private String id;
+
+    @NotBlank(message = "Chưa nhập họ và tên")
     private String fullname;
 
     @Builder.Default
     private String photo = "photo.jpg";
 
-    @Builder.Default
+    @NotNull(message = "Chưa chọn giới tính")
     private Boolean gender = true;
 
-    @Builder.Default
-    @DateTimeFormat(pattern = "MM/dd/yyyy")
-    private Date birthday = new Date();
 
-    @Builder.Default
-    private double salary = 12345.6789;
+    @NotNull(message = "Chưa nhập ngày sinh")
+    @Past(message = "Ngày sinh không hợp lệ")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date birthday;
 
-    @Builder.Default
+    @Min(value = 1000, message = "Lương tối thiểu phải là 1000")
+    @NotNull(message = "Chưa nhập lương")
+    private double salary;
+
     private Integer level = 0;
 }
