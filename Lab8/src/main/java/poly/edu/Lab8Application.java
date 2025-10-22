137ffc58efff69c5373643e1358580c0c2eda42e
iris2108 <iris2107007@gmail.com>
2025-10-22 10:51:28 +0700

    TS00916 - Lê Vân Như - Lab8

--- Lab8/src/main/java/poly/edu/Lab8Application.java
+++ Lab8/src/main/java/poly/edu/Lab8Application.java
@@ -1,9 +1,11 @@
-package com.poly.lab8;
+package poly.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Lab8Application {
 
     public static void main(String[] args) {
